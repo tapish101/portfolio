ee3f48318e713d5a6f190c6e5ba79d98e5544fa1
Tapish Talan <85446029+tapish101@users.noreply.github.com>
2023-04-24 19:47:19 +0530

    Add files via upload

---
+++ game/game_script.js
@@ -0,0 +1,82 @@
+import Ball from "./ball.js"
+import Paddle from "./paddle.js"
+
+const ball = new Ball(document.getElementById("ball"))
+const playerPaddle = new Paddle(document.getElementById("player-paddle"))
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
+const playerScoreElem = document.getElementById("player-score")
+const computerScoreElem = document.getElementById("computer-score")
+
+
+let lastTime
+
+function update(time) {
+  if (lastTime != null) {
+    const delta = time - lastTime
+    ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()])
+    computerPaddle.update(delta, ball.y)
+    const hue = parseFloat(
+      getComputedStyle(document.documentElement).getPropertyValue("--hue")
+    )
+
+    document.documentElement.style.setProperty("--hue", hue + delta * 0.01)
+
+    if (isLose()) handleLose()
+  }
+
+  lastTime = time
+  window.requestAnimationFrame(update)
+}
+
+function isLose() {
+  const rect = ball.rect()
+  return rect.right >= window.innerWidth || rect.left <= 0
+}
+
+function handleLose() {
+  const rect = ball.rect()
+  if (rect.right >= window.innerWidth) {
+    computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1
+  } else {
+    playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+  }
+  ball.reset()
+  computerPaddle.reset()
+
+  
+}
+
+document.addEventListener("mousemove", e => {
+  playerPaddle.position = (e.y / window.innerHeight) * 100
+})
+
+window.requestAnimationFrame(update)
+
+document.getElementById("mode").addEventListener("click", change_mode);
+
+function change_mode(){
+
+  const mode=document.getElementById("msg")
+
+  if (mode.textContent=="Easy"){
+    mode.textContent="Medium"
+  }
+  else if (mode.textContent=="Medium"){
+    mode.textContent="Hard"
+  }
+  else if (mode.textContent=="Hard"){
+    mode.textContent="Auto"
+  }
+  else if (mode.textContent=="Auto"){
+    mode.textContent="Easy"
+  }
+  else{
+    mode.textContent="Easy"
+  }
+
+  mode.style.display='grid'
+
+  setTimeout(() => {
+         mode.style.display='none';
+        },2000);
+}
